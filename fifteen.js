ab7a6d532ae12d5a8471675ba9690a8aa2b27ca0
ShemarSinclairennis <shemar.ennis@gmail.com>
2018-11-05 23:48:23 -0500

    Appearance code added

--- fifteen.js
+++ fifteen.js
@@ -1,3 +1,33 @@
 //Author: Shemar Ennis
 //ID#:620111861
 
+window.onload =() =>
+
+{
+	let p_area = document.getElementById("puzzlearea");
+	let children = document.querySelectorAll("#puzzlearea div")
+
+
+	let x=0;
+	let y =0;
+	let count =0;
+
+	for (let i=0; i< children.length; i++)
+		{
+
+			children[i].classList.add("puzzlepiece");
+
+			children[i].style.left = x+"px";  
+			children[i].style.top = y+"px";
+			children[i].style.backgroundPosition = `${-x}px ${-y}px`;
+			x +=100;
+			count +=1;
+
+			if (count%4==0){
+
+				y+=100;
+				x=0;
+			}
+			
+		  }
+}
